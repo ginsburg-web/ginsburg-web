0e3ecbce5bc54794983b54c51d7414ad09ec0d9a
H.M.P.H.Bhagya <72595176+Piumi274@users.noreply.github.com>
2021-06-08 15:00:21 +0530

    validation form

---
+++ validation.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+//database connect
+
+$con = mysqli_connect('localhost','root','',);
+
+mysqli_select_db($con,'userregistration');
+
+$name = $_POST['user'];
+$pass = $_POST['password'];
+//table
+$s = "select * from usertable where name ='$name' && password ='$pass' ";
+
+$result = mysqli_query($con,$s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+
+    $_SESSION['username'] = $name;
+    header('location:home.php');
+
+}else{
+  header('location:login.php');
+}
+
+?>
